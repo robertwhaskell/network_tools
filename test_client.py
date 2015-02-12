2ca939fa5ec59c766ecbe3b1b443850c12457dca
Robert Haskell <robertwhaskell@gmail.com>
2015-02-12 00:33:53 -0800

    wrote some tests. Now lets pass 'em.

--- test_client.py
+++ test_client.py
@@ -1,5 +1,5 @@
 import echo_client
-
+import os
 
 def test_HTTP_bad_response_error():
     msg = echo_client.start_client("""
@@ -43,3 +43,49 @@ def test_HTTP_version_error():
     body = "ERROR 505, HTTP/1.0 NOT SUPPORTED\r\n"
     response = "{}{}{}".format(error_response, headers, body)
     assert msg == response
+
+
+def test_HTTP_has_root():
+    msg = echo_client.start_client("""
+            GET webroot HTTP/1.0\r\nHost: localhost:50001\r\n
+            """)
+    okay_response = "HTTP/1.1 200 OK\r\n"
+    headers = "Content-Type: text/html\r\n"
+    body = """
+    <h1>webroot</h1>
+    <ul>
+        <li><b>images</b></li>
+        <li>a_web_page.html</li>
+        <li>sample.txt</li>
+    </ul>
+        """
+    response = "{}{}{}\r\n".format(okay_response, headers, body)
+    assert msg == response
+
+
+def test_HTTP_returns_webpage():
+    msg = echo_client.start_client("""
+            GET webroot/a_web_page.html HTTP/1.0\r\nHost: localhost:50001\r\n
+            """)
+    okay_response = "HTTP/1.1 200 OK\r\n"
+    headers = "Content-Type: text/html\r\n"
+    body = os.path('webroot/a_web_page')
+    response = "{}{}{}\r\n".format(okay_response, headers, body)
+    assert msg == response
+
+
+def test_HTTP_returns_dynamic_error():
+    error_response = "HTTP/1.1 400 ERROR\r\n"
+    headers = "Content-Type: text/plain\r\n"
+    body = "ERROR 400, BAD REQUEST\r\n"
+    response = "{}{}{}".format(error_response, headers, body)
+
+    request_list = ["""
+            POST webroot HTTP/1.0\r\nHost: localhost:50001\r\n
+            """, """
+            GET webroot/non-thing HTTP/1.0\r\nHost: localhost:50001\r\n
+            """, """
+            GET webroot HTTP/500.0\r\nHost: localhost:50001\r\n
+            """]
+    for req in request_list:
+        assert echo_client(req) != response
